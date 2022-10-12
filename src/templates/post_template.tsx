bf8dfce6b1e60757750f10ed91e3239b535d4818
KimJunSik <pkwaw@naver.com>
2022-10-12 23:47:36 +0900

    github CommentWidget 구현중

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,9 +1,11 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
-import { PostFrontmatterType, PostPageItemType } from 'types/PostItem.types'
+import { PostFrontmatterType } from 'types/PostItem.types'
 
 import Template from 'components/Common/Template'
 import PostHead from 'components/Post/PostHead'
+import PostContent from 'components/Post/PostContent'
+import CommentWidget from 'components/Post/CommentWidget'
 
 type PostTemplateProps = {
   data: {
@@ -48,6 +50,8 @@ const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
         categories={categories}
         thumbnail={gatsbyImageData}
       />
+      <PostContent html={html} />
+      <CommentWidget />
     </Template>
   )
 }

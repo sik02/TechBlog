2c20e2ff475367886f0c8e14c0d4f14a945562f9
KimJunSik <pkwaw@naver.com>
2022-10-14 03:03:26 +0900

    issue repo 변경

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -2,7 +2,7 @@ import React, { createRef, FunctionComponent, useEffect } from 'react'
 import styled from '@emotion/styled'
 
 const src = 'https://utteranc.es/client.js'
-const repo = 'sik02/TechBlog' // 자신 계정의 레포지토리로 설정
+const repo = 'sik02/sik02.github.io' // 자신 계정의 레포지토리로 설정
 
 type UtterancesAttributesType = {
   src: string
